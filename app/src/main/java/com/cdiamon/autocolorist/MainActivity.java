2bb07b0b54735ffbe28b9768daedbed6ea3b4c0a
Dmitriy Pavlukhin <padmitriy@gmail.com>
2016-12-09 18:15:21 +0400

    + oswald, + fragments, some changes

--- app/src/main/java/com/cdiamon/autocolorist/MainActivity.java
+++ app/src/main/java/com/cdiamon/autocolorist/MainActivity.java
@@ -1,9 +1,14 @@
 package com.cdiamon.autocolorist;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,31 +21,27 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.cdiamon.autocolorist.fragments.GalleryFragment;
+import com.cdiamon.autocolorist.fragments.OsvaldFragment;
+import com.cdiamon.autocolorist.fragments.TablesFragment;
+
+import static android.R.attr.fragment;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
-    Button btnGallery = (Button) findViewById(R.id.buttonCatalogue);
-
-    @Override
-    public void setContentView(View view) {
-        super.setContentView(view);
-        btnGallery.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), CarCodesGallery.class));
-            }
-        });
-    }
+        implements NavigationView.OnNavigationItemSelectedListener, TablesFragment.OnFragmentInteractionListener, GalleryFragment.OnFragmentInteractionListener, OsvaldFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        setContentView(R.layout.content_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        //TODO: is necessary?? http://stackoverflow.com/questions/28133600/set-initial-fragment-on-startup/34856256#34856256
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        tx.replace(R.id.container, new OsvaldFragment());
+        tx.commit();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -56,19 +57,20 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+        //first time..
+        drawer.openDrawer(GravityCompat.START);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-//        btnGallery.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//
-//            public void onClick(View v) {
-//                startActivity(new Intent(getApplicationContext(), CarCodesGallery.class));
-//            }
-//        });
     }
+
+
+    //TODO: isnecessary??? http://stackoverflow.com/questions/28133600/set-initial-fragment-on-startup/34856256#34856256
+//    public void setFragment(Fragment fragment) {
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        fragmentManager.beginTransaction().replace(R.id.container, new OsvaldFragment()).commit();
+//    }
 
     @Override
     public void onBackPressed() {
@@ -96,35 +98,63 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            // startActivity(new Intent(getApplicationContext(), WHAT.class));
+            Toast.makeText(getApplicationContext(), "Переходим в настройки", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.menu_calculator) {
+            // startActivity(new Intent(getApplicationContext(), WHAT.class));
+            Toast.makeText(getApplicationContext(), "Переходим в калькулятор", Toast.LENGTH_SHORT).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        //here create new fragment
+        Fragment fragment = null;
+        Class fragmentClass = null;
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            fragmentClass = OsvaldFragment.class;
             Toast.makeText(getApplicationContext(), "Переходим в 1", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_gallery) {
-
-            startActivity(new Intent(getApplicationContext(), CarCodesGallery.class));
+            fragmentClass = GalleryFragment.class;
             Toast.makeText(getApplicationContext(), "Переходим в галерею", Toast.LENGTH_SHORT).show();
-
         } else if (id == R.id.nav_slideshow) {
+            fragmentClass = TablesFragment.class;
             Toast.makeText(getApplicationContext(), "Переходим в 3", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_manage) {
+            fragmentClass = OsvaldFragment.class;
             Toast.makeText(getApplicationContext(), "Переходим в 4", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_share) {
+            fragmentClass = OsvaldFragment.class;
             Toast.makeText(getApplicationContext(), "Переходим в 5", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_send) {
+            fragmentClass = OsvaldFragment.class;
             Toast.makeText(getApplicationContext(), "Переходим в 6", Toast.LENGTH_SHORT).show();
         }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //insert new fragment, changing previous
+        FragmentManager fragmentmanager = getSupportFragmentManager();
+        fragmentmanager.beginTransaction().replace(R.id.container, fragment).commit();
+        //now hoghlighting selected menu in left bar
+        item.setChecked(true);
+        //show selected item name in top
+        setTitle(item.getTitle());
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -134,8 +164,11 @@ public class MainActivity extends AppCompatActivity
     public MainActivity() {
 
 
-
     }
 
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
