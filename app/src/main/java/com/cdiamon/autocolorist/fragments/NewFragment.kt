25a9f63e0029187b71ecf99b902590e855a5a31c
Dmitriy Pavlukhin <padmitriy@ozon.ru>
2020-12-13 17:34:23 +0400

    + viewbinding + refactoring + bugfixing

--- app/src/main/java/com/cdiamon/autocolorist/fragments/NewFragment.kt
+++ app/src/main/java/com/cdiamon/autocolorist/fragments/NewFragment.kt
@@ -6,39 +6,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.cdiamon.autocolorist.R
+import com.cdiamon.autocolorist.databinding.FragmentNewBinding
 
 class NewFragment : Fragment() {
-    private var mListener: OnFragmentInteractionListener? = null
+
+    private var _binding: FragmentNewBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_new, container, false)
+        _binding = FragmentNewBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    fun onButtonPressed(uri: Uri) {
-        if (mListener != null) {
-            mListener!!.onFragmentInteraction(uri)
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.inProgressTv.text = getText(R.string.fragmentOnConstruction)
+        binding.inProgressTv.setTextColor(
+                ContextCompat.getColor(requireContext(), android.R.color.holo_orange_dark))
     }
 
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            mListener = context
-        } else {
-            throw RuntimeException(context!!.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        mListener = null
-    }
-
-    interface OnFragmentInteractionListener {
-        fun onFragmentInteraction(uri: Uri)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
